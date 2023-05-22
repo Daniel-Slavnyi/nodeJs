4e0c8ffaf4f249035843b708a91e4174087c7f3c
Daniel-Slavnyi <dslavnyj00@gmail.com>
2023-05-22 17:17:22 +0300

    study of module for node.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("Hello Node.js i know how to create project");
+const { admin, clients } = require("./users/users");
+
+console.log("Hello Node.js i know how to create project and i am profeshional");
+console.log("users", admin, clients);
---
+++ users/users.js
@@ -0,0 +1,6 @@
+const admin = ["Alex", "Mango", "Orange"];
+const clients = ["Jorgio", "Anna", "Bill"];
+
+const users = { admin, clients };
+
+module.exports = users;
