546aae966b9c5ec3733c544bb194bab35bb365ed
Daniel-Slavnyi <dslavnyj00@gmail.com>
2023-05-24 18:53:25 +0300

    study fs.readFile/fs.appendFile/fs.writeFile

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require("fs/promises")
+
 const { admin, clients } = require("./users/users");
 const {isLeapYear} = require("./date")
 const getCurrentMonth = require("./date").getCurrentMonth()
@@ -6,4 +8,22 @@ console.log("Hello Node.js i know how to create project and i am profeshional");
 console.log("users", admin, clients);
 
 
-isLeapYear(getCurrentMonth)
+isLeapYear(getCurrentMonth);
+
+const readFile = async () => {
+    const data = await fs.readFile("./files/files.txt", "utf-8")
+
+    console.log("data", data);
+}
+
+const addText = async () => {
+    const data = await fs.appendFile("./files/files.txt", "\nCongritulate, you can add text")
+}
+
+const replaceText = async () => {
+    const data = await fs.writeFile("./files/files.txt", "Wow , that is great, well done ")
+}
+
+addText()
+replaceText()
+readFile()
