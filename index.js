2191dd08ab95e8ef9163b3dcda52911e0d109bad
Daniel-Slavnyi <dslavnyj00@gmail.com>
2023-05-30 14:30:45 +0300

    add .json db

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const fs = require("fs/promises")
+// const fs = require("fs/promises")
 
-const { admin, clients } = require("./users/users");
-const {isLeapYear} = require("./date")
-const getCurrentMonth = require("./date").getCurrentMonth()
+// const { admin, clients } = require("./users/users");
+// const {isLeapYear} = require("./date")
+// const getCurrentMonth = require("./date").getCurrentMonth()
 
-console.log("Hello Node.js i know how to create project and i am profeshional");
-console.log("users", admin, clients);
+// console.log("Hello Node.js i know how to create project and i am profeshional");
+// console.log("users", admin, clients);
 
 
-isLeapYear(getCurrentMonth);
+// isLeapYear(getCurrentMonth);
 
-const readFile = async () => {
-    const data = await fs.readFile("./files/files.txt", "utf-8")
+// const readFile = async () => {
+//     const data = await fs.readFile("./files/files.txt", "utf-8")
 
-    console.log("data", data);
-}
+//     console.log("data", data);
+// }
 
-const addText = async () => {
-    const data = await fs.appendFile("./files/files.txt", "\nCongritulate, you can add text")
-}
+// const addText = async () => {
+//     const data = await fs.appendFile("./files/files.txt", "\nCongritulate, you can add text")
+// }
 
-const replaceText = async () => {
-    const data = await fs.writeFile("./files/files.txt", "Wow , that is great, well done ")
-}
+// const replaceText = async () => {
+//     const data = await fs.writeFile("./files/files.txt", "Wow , that is great, well done ")
+// }
 
-addText()
-replaceText()
-readFile()
+// addText()
+// replaceText()
+// readFile()
