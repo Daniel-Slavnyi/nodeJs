bfc46bf32eee6fa817cdb5cae69fcf7d772b99a3
Daniel-Slavnyi <dslavnyj00@gmail.com>
2023-06-06 22:18:36 +0300

    study express

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,10 +1,30 @@
 const express = require("express");
-const contacts = require("../../contacts/contacts");
+const contacts = require("../../contacts");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json(contacts);
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await contacts.getAll();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const {id} = req.params;
+    const result = await contacts.getContactById(id)
+    
+    if(!result) {
+      throw new Error();
+    }
+
+    res.json(result)
+  } catch (error) {
+    next(error)
+  }
+})
 
 module.exports = router;
